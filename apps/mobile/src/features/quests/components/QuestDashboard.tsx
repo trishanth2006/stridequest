92cfad89313585f64923a97d84c62be3a73ec14e
Trishanth-reddy <strishanthreddy@gmail.com>
2026-07-01 14:48:51 +0530

    fix(quests): import React for ReactNode type in QuestDashboard

--- apps/mobile/src/features/quests/components/QuestDashboard.tsx
+++ apps/mobile/src/features/quests/components/QuestDashboard.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { View, Text, ScrollView, Pressable } from 'react-native'
 import Animated, {
   useSharedValue,
